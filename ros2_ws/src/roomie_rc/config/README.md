0460ecd8ee881ce8f7fd7d0efd08e157fc77444c
박효진 <damuya07@gmail.com>
2025-07-24 07:08:59 +0900

    [RC] 기본 구조 정리

--- ros2_ws/src/roomie_rc/config/README.md
+++
@@ -1,8 +0,0 @@
-# Configuration Files
-
-roomie_rc 패키지 설정 파일들
-
-## 예정된 파일들
-- `rc_params.yaml` - 로봇 제어 파라미터
-- `navigation_params.yaml` - 내비게이션 설정
-- `hardware_config.yaml` - 하드웨어 설정
---
+++ ros2_ws/src/roomie_rc/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+package_name = 'roomie_rc'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Roomie Team',
+    maintainer_email='admin@roomie.com',
+    description='Robot Controller for Roomie service robot system',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'rc_node = roomie_rc.rc_node:main',
+        ],
+    },
+)
