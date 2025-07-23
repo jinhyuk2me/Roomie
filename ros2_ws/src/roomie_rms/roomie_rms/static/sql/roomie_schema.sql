b7e6139846ec526ad56863c9ddf1368b069d3cb9
heyjay1002 <heyjay1002@gmail.com>
2025-07-24 00:37:16 +0900

    [rms] 서버 관련 파일 추가

---
+++ ros2_ws/src/roomie_rms/roomie_rms/static/sql/roomie_schema.sql
@@ -0,0 +1,113 @@
+
+-- roomie DB Table Definitions
+
+CREATE TABLE floor (
+    id INT PRIMARY KEY,
+    name VARCHAR(8)
+);
+
+CREATE TABLE location_type (
+    id INT PRIMARY KEY,
+    name VARCHAR(16)
+);
+
+CREATE TABLE location (
+    id INT PRIMARY KEY,
+    name VARCHAR(16),
+    location_type_id INT,
+    is_destination BOOLEAN,
+    floor_id INT,
+    location_x FLOAT,
+    location_y FLOAT,
+    FOREIGN KEY (location_type_id) REFERENCES location_type(id),
+    FOREIGN KEY (floor_id) REFERENCES floor(id)
+);
+
+CREATE TABLE task_type (
+    id INT PRIMARY KEY,
+    name VARCHAR(16)
+);
+
+CREATE TABLE task_status (
+    id INT PRIMARY KEY,
+    name VARCHAR(16)
+);
+
+CREATE TABLE robot_status (
+    id INT PRIMARY KEY,
+    name VARCHAR(16)
+);
+
+CREATE TABLE error (
+    id INT PRIMARY KEY,
+    name VARCHAR(16)
+);
+
+CREATE TABLE task (
+    id INT PRIMARY KEY,
+    type_id INT,
+    task_status_id INT,
+    location_id INT,
+    robot_id INT,
+    task_creation_time DATETIME,
+    robot_assignment_time DATETIME,
+    pickup_completion_time DATETIME,
+    delivery_arrival_time DATETIME,
+    task_completion_time DATETIME,
+    task_cancellation_time DATETIME
+);
+
+CREATE TABLE robot (
+    id INT PRIMARY KEY,
+    name VARCHAR(16),
+    model_name VARCHAR(16),
+    robot_status_id INT,
+    task_id INT,
+    floor_id INT,
+    battery_status INT,
+    error_id INT,
+    FOREIGN KEY (robot_status_id) REFERENCES robot_status(id),
+    FOREIGN KEY (task_id) REFERENCES task(id),
+    FOREIGN KEY (floor_id) REFERENCES floor(id),
+    FOREIGN KEY (error_id) REFERENCES error(id)
+);
+
+CREATE TABLE `order` (
+    id INT PRIMARY KEY,
+    task_id INT,
+    location_id INT,
+    total_price INT,
+    FOREIGN KEY (task_id) REFERENCES task(id),
+    FOREIGN KEY (location_id) REFERENCES location(id)
+);
+
+CREATE TABLE food (
+    id INT PRIMARY KEY,
+    name VARCHAR(16),
+    price INT,
+    image VARCHAR(255)
+);
+
+CREATE TABLE food_order_item (
+    id INT PRIMARY KEY,
+    order_id INT,
+    food_id INT,
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES `order`(id),
+    FOREIGN KEY (food_id) REFERENCES food(id)
+);
+
+CREATE TABLE supply (
+    id INT PRIMARY KEY,
+    name VARCHAR(16),
+    image VARCHAR(255)
+);
+
+CREATE TABLE supply_order_item (
+    id INT PRIMARY KEY,
+    order_id INT,
+    supply_id INT,
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES `order`(id),
+    FOREIGN KEY (supply_id) REFERENCES supply(id)
+);
